fbd21366bce2bc44c135a4f3aff3bcc5dbf46326
Darren Behan <darrenbehan@hotmail.com>
2020-09-01 01:05:35 +1000

    adding first api-route, GET request to return all workouts from DB

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,16 @@
-// Requiring our Todo model
+// Requiring our index model
 var db = require("../models");
 
 // Routes
 module.exports = function(app) {
-
+  // This request returns all workouts
+  app.get('/api/workouts', async (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 };
